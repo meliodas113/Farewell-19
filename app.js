a5f971ef7edb05d0bdc93f8dfa9d42675b9739a2
Akshit Patel <akshitpatel72@gmail.com>
2019-04-15 13:53:08 +0530

    css animate added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let nameContainer = document.querySelector(".names");
 let deptContainer = document.querySelector(".dept");
 let name = ["Megh Thakkar", "Amritanshu Jain"];
 let department = ["Backend", "x-Backend"];
+var photoDiv = document.querySelectorAll(".photo")[0];
 function doNext() {
     if(objectIndex != numOfImages)
     objectIndex++;
@@ -24,4 +25,5 @@ function changeDetails() {
     nameContainer.innerHTML = name[objectIndex-1];
     deptContainer.innerHTML = department[objectIndex-1];
     imageContainer.src = "assests/" + objectIndex + ".png";
+    photoDiv.style.animation = "animate 1s ease forwards";   
 }
