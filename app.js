e98c771fc3881c38dea5f42b8edcf332b2f1a02f
mukul0596 <icecoolmkl@gmail.com>
2019-04-15 13:34:16 +0530

    added name and department changing functionality

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-let imageIndex = 1;
+let objectIndex = 1;
 let numOfImages = 2;
-let container = document.querySelector(".image-container");
+let imageContainer = document.querySelector(".image-container");
+let nameContainer = document.querySelector(".names");
+let deptContainer = document.querySelector(".dept");
+let name = ["Megh Thakkar", "Amritanshu Jain"];
+let department = ["Backend", "x-Backend"];
 function doNext() {
-    if(imageIndex != numOfImages)
-    imageIndex++;
+    if(objectIndex != numOfImages)
+    objectIndex++;
     else
-    imageIndex = 1;
-    container.src = "assests/" + imageIndex + ".png";
+    objectIndex = 1;
+    changeDetails();
 }
 function doPrev() {
 
-    if(imageIndex != 1)
-    imageIndex--;
+    if(objectIndex != 1)
+    objectIndex--;
     else
-    imageIndex = numOfImages;
-    container.src = "assests/" + imageIndex + ".png";
+    objectIndex = numOfImages;
+    changeDetails();
+}
+function changeDetails() {
+    nameContainer.innerHTML = name[objectIndex-1];
+    deptContainer.innerHTML = department[objectIndex-1];
+    imageContainer.src = "assests/" + objectIndex + ".png";
 }
